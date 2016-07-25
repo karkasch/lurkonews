3579448405c6ff190dc0750d95dcddc6684b42e3
karkasch <nutzer12@gmail.com>
2016-07-25 21:11:55 +0200

    current state

---
+++ Lurkonews/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lurkonews.Startup))]
+namespace Lurkonews
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
